7ba123ccb0a0610c657ee5b358282b475f3343df
NoufSaleh20 <129336340+NoufSaleh20@users.noreply.github.com>
2023-08-04 01:58:13 +0300

    Add files via upload

---
+++ arduino1 code.ino
@@ -0,0 +1,22 @@
+
+
+#include <Wire.h>
+
+int pushButton = A0;
+
+int x = 1;
+
+void setup() {
+  
+  Wire.begin();
+  pinMode(pushButton, INPUT);
+}
+
+void loop(){
+  
+   Wire.beginTransmission(1);
+   x = digitalRead(pushButton);
+   Wire.write(x);
+   Wire.endTransmission();
+   delay(500);
+}
